5e3cb78cd30886cc7e735256ca9efba4667841f9
Антон Волов <volovanton@mail.ru>
2021-11-29 12:43:30 +0300

    Add project files.

---
+++ Net Diversity WPF/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Net_Diversity_Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Net_Diversity_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            Label1.Content = string.Concat("Hello ", TextBox1.Text, '!');
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            Label2.Content = Greetings.Hello(TextBox2.Text);
+        }
+    }
+}
---
+++ Net Diversity/Program.cs
@@ -0,0 +1,16 @@
+﻿using Net_Diversity_Library;
+using System;
+
+namespace Net_Diversity
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please, type your name for task 1:");
+            Console.WriteLine(string.Concat("Hello ", Console.ReadLine(), '!'));
+            Console.WriteLine("Please, type your name for task 2:");
+            Console.WriteLine(Greetings.Hello(Console.ReadLine()));
+        }
+    }
+}
