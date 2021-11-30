d7d321e5dc72efd49aa3202f3beb17c1662b7da6
Антон Волов <volovanton@mail.ru>
2021-11-30 14:05:04 +0300

    Add framework project

---
+++ NET Diversity WPF Framework/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Net_Diversity_Library_Framework;
+using System.Windows;
+
+namespace NET_Diversity_WPF_Framework
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            Label2.Content = Greetings.Hello(TextBox2.Text);
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            Label1.Content = string.Concat("Hello ", TextBox1.Text, '!');
+        }
+    }
+}
---
+++ Net Diversity Library Framework/Greetings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net_Diversity_Library_Framework
+{
+    public static class Greetings
+    {
+        public static string Hello(string username)
+        {
+            return string.Concat(DateTime.Now.ToString("h:mm:ss tt"), "Hello ", username, '!');
+        }
+    }
+}
