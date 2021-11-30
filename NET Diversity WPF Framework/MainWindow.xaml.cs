36458164a9e26548ef489483b5933aa2ab047c43
Антон Волов <volovanton@mail.ru>
2021-11-30 22:11:41 +0300

    add placeholder

--- NET Diversity WPF Framework/MainWindow.xaml.cs
+++ NET Diversity WPF Framework/MainWindow.xaml.cs
@@ -22,5 +22,37 @@ namespace NET_Diversity_WPF_Framework
         {
             Label1.Content = string.Concat("Hello ", TextBox1.Text, '!');
         }
+
+        private void TextBox3_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox3.Visibility = Visibility.Collapsed;
+            TextBox1.Visibility = Visibility.Visible;
+            TextBox1.Focus();
+        }
+
+        private void TextBox1_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(TextBox1.Text))
+            {
+                TextBox1.Visibility = Visibility.Collapsed;
+                TextBox3.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void TextBox2_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(TextBox2.Text))
+            {
+                TextBox2.Visibility = Visibility.Collapsed;
+                TextBox4.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void TextBox4_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox4.Visibility = Visibility.Collapsed;
+            TextBox2.Visibility = Visibility.Visible;
+            TextBox2.Focus();
+        }
     }
 }
