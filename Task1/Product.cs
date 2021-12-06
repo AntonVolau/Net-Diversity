4edfd820e44aecba134277c042f939f50d1409b8
Антон Волов <volovanton@mail.ru>
2021-12-06 13:26:42 +0300

    Implement task solution

---
+++ Task1/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Product
+    {
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public override bool Equals(Object obj)
+        {
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                Product p = (Product)obj;
+                return (Name == p.Name) && (Price == p.Price);
+            }
+        }
+    }
+}
