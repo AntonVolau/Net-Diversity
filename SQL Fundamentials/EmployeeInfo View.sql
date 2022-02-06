d5f64e736418158b5e3aeda9e4518f1319fa63c5
Антон Волов <volovanton@mail.ru>
2022-02-07 02:02:24 +0300

    View fix

--- SQL Fundamentials/EmployeeInfo View.sql
+++ SQL Fundamentials/EmployeeInfo View.sql
@@ -1,14 +1,11 @@
-CREATE VIEW EmployeeInfo AS
-SELECT Employee.Id AS EmployeeId, 
-        CASE WHEN Employee.EmployeeName IS NOT NULL 
-       THEN EmployeeName
-       ELSE (Person.FirstName + ' ' + Person.LastName)
-	   END AS EmployeeFullName,
-        CONCAT((SELECT Address.ZipCode FROM Address WHERE Employee.AddressId = Address.Id), '_', 
-		(SELECT Address.State FROM Address WHERE Employee.AddressId = Address.Id), ', ', 
-		(SELECT Address.City FROM Address WHERE Employee.AddressId = Address.Id), '-',
-		(SELECT Address.Street FROM Address WHERE Employee.AddressId = Address.Id)) As EmployeeFullAddress,
-		CONCAT((SELECT Employee.CompanyName FROM Employee WHERE Employee.AddressId = Address.Id), '(', 
-		(SELECT Employee.Position FROM Employee WHERE Employee.AddressId = Address.Id), ')') As EmployeeCompanyInfo
-FROM Employee INNER JOIN Address ON Employee.AddressId = Address.Id
-INNER JOIN Person ON Person.Id = Employee.PersonId
+CREATE VIEW [dbo].[EmployeeInfo3]
+	AS SELECT TOP(100) e.Id,
+	COALESCE(EmployeeName, CONCAT(FirstName,' ',LastName)) EmployeeFullName,
+	CONCAT(ZipCode, '_', State, ',', City, '-', Street) EmployeeFullAddress,
+	CONCAT (CompanyName, '(', Position, ')') EmployeeCompanyInfo
+	FROM Employee e
+	JOIN Person p 
+	ON p.Id = e.PersonId
+	JOIN Address a 
+	ON a.Id = e.AddressId
+	ORDER BY CompanyName, City ASC
