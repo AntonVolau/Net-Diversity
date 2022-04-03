40d6d5ed4c2245c6ec8bf825bdd0b7ef62af72c8
Антон Волов <volovanton@mail.ru>
2022-04-03 22:58:51 +0300

    Optional tasks implemented

---
+++ MVC/Data/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+#nullable disable
+
+namespace Data.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+
+        [JsonIgnore]
+        public byte[] Picture { get; set; }
+
+        [JsonIgnore]
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
---
+++ WebAPI/WebAPI/Startup.cs
@@ -0,0 +1,50 @@
+using Data.Configuration;
+using Data.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.Configure<DatabaseConfiguration>(Configuration.GetSection(nameof(DatabaseConfiguration)));
+            services.AddDbContext<NorthwindContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetValue<string>("DatabaseConfiguration:ConnectionString"));
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
